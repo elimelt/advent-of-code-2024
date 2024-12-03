87df7eb6694582433e5c75b26c559ef1347e7042
Elijah Melton <elimelt@uw.edu>
2024-12-02 21:33:04 -0800

    day 2 and 3

--- src/day02/sol.py
+++ src/day02/sol.py
@@ -1,7 +1,54 @@
 from typing import List
 
-def part_one(data: List[str]) -> int:
-    return len(data)
 
-def part_two(data: List[str]) -> int:
-    return len(data)
+def is_safe_sequence(levels: List[int]) -> bool:
+    if len(levels) < 2:
+        return True
+
+    increasing = levels[1] > levels[0]
+
+    for i in range(len(levels) - 1):
+        diff = levels[i + 1] - levels[i]
+
+        if abs(diff) < 1 or abs(diff) > 3:
+            return False
+        if increasing and diff < 0:
+            return False
+        if not increasing and diff > 0:
+            return False
+
+    return True
+
+
+def part_one(lines: List[str]) -> int:
+    safe_count = 0
+
+    for line in lines:
+        levels = [int(x) for x in line.strip().split()]
+        if is_safe_sequence(levels):
+            safe_count += 1
+
+    return safe_count
+
+
+def is_safe_with_dampener(levels: List[int]) -> bool:
+    if is_safe_sequence(levels):
+        return True
+
+    for i in range(len(levels)):
+        dampened_levels = levels[:i] + levels[i + 1 :]
+        if is_safe_sequence(dampened_levels):
+            return True
+
+    return False
+
+
+def part_two(lines: List[str]) -> int:
+    safe_count = 0
+
+    for line in lines:
+        levels = [int(x) for x in line.strip().split()]
+        if is_safe_with_dampener(levels):
+            safe_count += 1
+
+    return safe_count
--- src/day03/sol.py
+++ src/day03/sol.py
@@ -1,7 +1,37 @@
+import re
 from typing import List
+from itertools import chain
+
+
+def parse_mul(line: str):
+    reg = r"mul\(\d+,\d+\)"
+    return re.findall(reg, line)
+
 
 def part_one(data: List[str]) -> int:
-    return len(data)
+    eqns = [mul[4:-1].split(",") for line in data for mul in parse_mul(line)]
+    return sum([int(eq[0]) * int(eq[1]) for eq in eqns])
+
+
+def parse_mul_do_dont(line: str):
+    rmul = r"mul\(\d+,\d+\)"
+    rdo = r"do\(\)"
+    rdont = r"don\'t\(\)"
+
+    res = re.findall(rf"{rmul}|{rdo}|{rdont}", line)
+    return res
+
 
 def part_two(data: List[str]) -> int:
-    return len(data)
+    enabled = True
+    res = 0
+    for expr in [exp for line in data for exp in parse_mul_do_dont(line)]:
+        if expr == "do()":
+            enabled = True
+        elif expr == "don't()":
+            enabled = False
+        elif "mul" in expr:
+            if enabled:
+                eq = expr[4:-1].split(",")
+                res += int(eq[0]) * int(eq[1])
+    return res
