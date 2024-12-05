3a9e56c7387faf2c21b7b104af8a68fe9ed4e493
Elijah Melton <elimelt@uw.edu>
2024-12-04 22:04:03 -0800

    day 4 5

--- src/day04/sol.py
+++ src/day04/sol.py
@@ -1,7 +1,66 @@
 from typing import List
 
+XMAS = ["X", "M", "A", "S"]
+
+
+def to_grid(lines: List[str]) -> List[List[str]]:
+    return [list(line) for line in lines]
+
+
+def check(grid: List[List[str]], x: int, y: int, dx: int, dy: int) -> bool:
+    n, m = len(grid), len(grid[0])
+
+    for i in range(len(XMAS)):
+        new_x, new_y = x + i * dx, y + i * dy
+        if new_x < 0 or new_x >= n or new_y < 0 or new_y >= m:
+            return False
+        if grid[new_x][new_y] != XMAS[i]:
+            return False
+    return True
+
+
+def search(grid: List[List[str]], x: int, y: int) -> int:
+    if grid[x][y] != "X":
+        return 0
+
+    directions = [
+        (0, 1),
+        (0, -1),
+        (1, 0),
+        (-1, 0),
+        (1, 1),
+        (-1, -1),
+        (1, -1),
+        (-1, 1),
+    ]
+
+    count = 0
+    for dx, dy in directions:
+        if check(grid, x, y, dx, dy):
+            count += 1
+    return count
+
+
 def part_one(data: List[str]) -> int:
-    return len(data)
+    grid = to_grid(data)
+    total = 0
+
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            total += search(grid, x, y)
+
+    return total
+
 
 def part_two(data: List[str]) -> int:
-    return len(data)
+    return sum(
+        [
+            int(
+                data[x][y] == "A"
+                and (data[x - 1][y - 1], data[x + 1][y + 1]) in (("M", "S"), ("S", "M"))
+                and (data[x + 1][y - 1], data[x - 1][y + 1]) in (("M", "S"), ("S", "M"))
+            )
+            for y in range(1, len(data[0]) - 1)
+            for x in range(1, len(data) - 1)
+        ]
+    )
--- src/day05/sol.py
+++ src/day05/sol.py
@@ -1,7 +1,78 @@
 from typing import List
+from collections import defaultdict
+
+
+def make_graph(data: List[str]) -> dict[str, list[str]]:
+    graph = defaultdict(list)
+    # indegree = defaultdict(int)
+    c = 0
+    for line in data:
+        c += 1
+        if line == "":
+            break
+        s, t = line.split("|")
+        graph[s].append(t)
+
+    return graph, c
+
+
+def make_orderings(data: List[str], c: int) -> list[list[str]]:
+    lines = data[c:]
+    return [line.split(",") for line in lines]
+
+
+def search(graph, path) -> bool:
+    indegrees = defaultdict(int)
+    ele = set(path)
+    for u in graph:
+        for v in graph[u]:
+            if u in ele and v in ele:
+                indegrees[v] += 1
+
+    i = 0
+    while ele:
+        curr = path[i]
+        if indegrees[curr] != 0:
+            return False
+        ele.remove(curr)
+        for v in graph[curr]:
+            indegrees[v] -= 1
+        i += 1
+    return True
+
 
 def part_one(data: List[str]) -> int:
-    return len(data)
+    graph, c = make_graph(data)
+    paths = make_orderings(data, c)
+
+    return sum(
+        int(path[len(path) // 2]) if search(graph, path) else 0 for path in paths
+    )
+
+def sort(graph, path):
+    indegrees = defaultdict(int)
+    ele = set(path)
+    for u in graph:
+        for v in graph[u]:
+            if u in ele and v in ele:
+                indegrees[v] += 1
+    while len(ele) > len(path)//2:
+        for e in ele:
+            if indegrees[e] == 0:
+                ele.remove(e)
+                for v in graph[e]:
+                    indegrees[v] -= 1
+                yield e
+                break
 
 def part_two(data: List[str]) -> int:
-    return len(data)
+    graph, c = make_graph(data)
+
+    return sum(
+        int(s[-1]) for s in [
+            list(sort(graph, path))
+                for path in make_orderings(data, c)
+                if not search(graph, path)
+        ]
+    )
+
